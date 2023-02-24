ca88cf0cfdf2a6877694ca294a4a60af7150e5f6
rus <r.r.samikov@mail.ru>
2023-02-24 14:51:17 +0300

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.ycombinator.com/item?id=34886766'
+
+def comm (url):
+
+    file = requests.get(url, stream=True)
+    filename = open(url.split('/')[-1], 'wb').write(file.content)
+
+    src = requests.get(url)
+    soup = BeautifulSoup(src.text, 'lxml')
+    comm = soup.findAll("span", class_="commtext c00")
+    print("All comments from url", url, comm)
+
+comm(url)
