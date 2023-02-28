91670306a71e3695733455a562599c964d29b556
rus <r.r.samikov@mail.ru>
2023-02-28 12:43:36 +0300

    init_post

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+from fastapi import FastAPI
 
-url = "https://news.ycombinator.com/item?id=34886766"
+# url = "https://news.ycombinator.com/item?id=34886766"
+
+app = FastAPI("Request_app")
 
 
-def comm(url):
+@app.post("/comm_f")
+async def comm_f(url: str):
     file = requests.get(url, stream=True)
     filename = open(url.split("/")[-1], "wb").write(file.content)
 
     src = requests.get(url)
     soup = BeautifulSoup(src.text, "lxml")
     comm = soup.findAll("span", class_="commtext c00")
-    print("All comments from url", url, comm)
+    # print("All comments from url", url, comm_f)
+    return comm
 
 
-comm(url)
+# comm_f(url)
