e5e6e422930a828fd291b45720fcb7a7a6d7d3b9
rus <r.r.samikov@mail.ru>
2023-03-06 08:34:52 +0300

    text

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 import json
 
-import fastapi
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from urllib.parse import urlparse
 
 url_1 = "https://news.ycombinator.com/item?id=34886766"
 
-app = FastAPI()
-
+# app = FastAPI()
 
 # @app.get("/comm_f")
+
 def comm_f(url):
-    src = requests.get(url)
-    soup = BeautifulSoup(src.text, "lxml")
+    # req = requests.get(url)
+    # src = req.text
+    # with open('index.html', 'w') as file:
+    #     file.write(src)
 
-    data = soup.findAll("td", class_="default")
-    # print(data)
-    id = data.find("a", class_="hnuser")
-    comm = data.find("span", class_="commtext c00")
-    text = {"id": "comm"}
+    with open("index.html") as file:
+        src = file.read()
 
-    # id = soup.findAll("a", class_="hnuser")
-    # comm = id.find("span", class_="commtext c00")
-    # for i in id:
-    #     comm = soup.find("span", class_="commtext c00")
-    #     print(comm)
-    #     print(i.text.strip())
-    # print(id)
-    comm_j = json.dumps(text)
-    # print(comm_j)
-    # print("All comments from url=", url, comm)
-    json_data = jsonable_encoder(comm_j)
-    return JSONResponse(content=json_data)
+    soup = BeautifulSoup(src.strip("https://"), "lxml")
+
+    url_1 = urlparse("https://news.ycombinator.com/item?id=34886766", scheme='', allow_fragments=True)
+    # print(url_1.query)
+    comm = soup.findAll("span", class_="commtext c00")
+
+    # print(comm)
+    text = {
+        "item_id" : url_1.query,
+        "comments": comm
+    }
+    # with open(f"json_data.json", "w", encoding = "utf-8") as file:
+    #     json.dump(text, file, indent=4,ensure_ascii=False)
+    print(text)
+
+
+    # json_data = jsonable_encoder(text)
+    # print(json_data)
+    # return JSONResponse(content=text)
 
 
 comm_f(url_1)
