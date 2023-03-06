377a2568ff081c3cdc1a91e2f53912ea4da4e267
rus <r.r.samikov@mail.ru>
2023-03-06 12:54:07 +0300

    response_json_1

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from urllib.parse import urlparse
 
 url_1 = "https://news.ycombinator.com/item?id=34886766"
@@ -31,17 +30,6 @@ def comm_f():
     comm = soup.findAll("span", class_="commtext c00")
     comm_1 = [i.text for i in comm]
 
-    # print(comm_1)
-    text = {"item_id": url_1.query, "comments": comm_1}
-    # with open(f"json_data.json", "w", encoding = "utf-8") as file:
-    #     json.dump(text, file, indent=4,ensure_ascii=False)
-    # print(text)
-
-    # json_data = jsonable_encoder(text)
-    # print(json_data)
-    # return JSONResponse({"item_id": url_1.query, "comments": comm_1})
     return {"item_id": url_1.query, "comments": comm_1}
-    # return JSONResponse(content=text)
-    # return JSONResponse(content=json_data)
 
 # comm_f(url_1)
