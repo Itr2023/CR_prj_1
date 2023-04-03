192bad0ea81ed23b2c8a895c08985e408b0c39dc
rus <r.r.samikov@mail.ru>
2023-04-03 09:36:18 +0300

    param_url

--- main.py
+++ main.py
@@ -6,30 +6,36 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from urllib.parse import urlparse
 
-url_1 = "https://news.ycombinator.com/item?id=34886766"
-
+url = "index.html"
 app = FastAPI()
 
+# url_1 = "https://news.ycombinator.com/item?id=34886766"
 
-@app.get("/")
-def comm_f():
-    # req = requests.get(url)
+
+@app.get(f"/comments/{url}")
+def comm_f(url):
+    # req = requests.get("https://news.ycombinator.com/item?id=34886766")
     # src = req.text
     # with open('index.html', 'w') as file:
     #     file.write(src)
 
     with open("index.html") as file:
+        # with open(url) as file:
         src = file.read()
 
     soup = BeautifulSoup(src, "lxml")
 
-    url_1 = urlparse(
-        "https://news.ycombinator.com/item?id=34886766", scheme="", allow_fragments=True
+    url = urlparse(
+        "index.html",
+        scheme="",
+        allow_fragments=True
+        # url, scheme="", allow_fragments=True
     )
     # print(url_1.query)
     comm = soup.findAll("span", class_="commtext c00")
     comm_1 = [i.text for i in comm]
 
-    return {"item_id": url_1.query, "comments": comm_1}
+    return [{"item_id": url.query, "comments": comm_1}]
 
-# comm_f(url_1)
+
+# comm_f()
